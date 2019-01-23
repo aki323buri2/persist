e038d55597a4645528219187cd1e532319974b6e
akimitsu nakajima <a-nakajima@shokuryu.co.jp>
2019-01-23 16:07:25 +0900

    .

---
+++ src/persist.js
@@ -0,0 +1,64 @@
+import { actions } from './actions';
+import { get, merge } from 'lodash';
+export const rootKey = 'goods-issue-weekly';
+export const whitelist = [
+    'params.syozok', 
+];
+export const loadReconcilers = {
+    'params.syozok': a => a === undefined ? null : parseInt(a), 
+};
+export const saveReconcilers = {
+};
+export const makeKey = key => 
+{
+    return`${rootKey}/${key}`;
+};
+export const load = (key, backup) => 
+{
+    return localStorage.getItem(makeKey(key), backup);
+};
+export const save = (key, value) => 
+{
+    return localStorage.setItem(makeKey(key), value);
+};
+export const purge = key => 
+{
+    return localStorage.removeItem(key ? makeKey(key) : rootKey);
+};
+export const pickValues = state => 
+{
+    return whitelist.reduce((values, key) => merge(values, {
+        [key]: get(state, key), 
+    }), {});
+};
+export const saveIfChanged = (before, after) => 
+{
+    for ([ key, value ] of Object.entries(after))
+    {
+        if (value !== before[key])
+        {
+            const reconciler = saveReconcilers[key] || (a => a);
+            save(key, reconciler(value));
+        }
+    }
+};
+let bootstrap = false;
+export const createPersistMiddleware = () => store => next => action => 
+{
+    if (bootstrap === false)
+    {
+        bootstrap = true;
+        for (const key of whitelist)
+        {
+            const action = get(actions, key);
+            const reconciler = loadReconcilers[key] || (a => a); 
+            const value = reconciler(load(key));
+            store.dispatch({ ...action(value), meta: 'rehydrade' });
+        }
+    }
+    const before = pickValues(store.getState());
+    next(action);
+    if (action.meta === 'rehydrade') return;
+    const after = pickValues(store.getState());
+    saveIfChanged(before, after);
+};
